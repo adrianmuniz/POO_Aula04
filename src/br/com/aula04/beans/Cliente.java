a2923fa34d9f6514d36ed8113a80584c2327b994
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-08-23 19:50:58 -0300

    Criando a classe Cliente

---
+++ src/br/com/aula04/beans/Cliente.java
@@ -0,0 +1,62 @@
+package br.com.aula04.beans;
+
+public class Cliente {
+	private String nome;
+	private String endereco;
+	private String cpf;
+	private String rg;
+	private String fone;
+	
+	public Cliente() {}
+
+	public Cliente(String nome, String endereco, String cpf, String rg, String fone) {
+		super();
+		this.nome = nome;
+		this.endereco = endereco;
+		this.cpf = cpf;
+		this.rg = rg;
+		this.fone = fone;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getFone() {
+		return fone;
+	}
+
+	public void setFone(String fone) {
+		this.fone = fone;
+	};
+	
+	
+}
---
+++ src/br/com/aula04/beans/ContaCorrente.java
@@ -0,0 +1,12 @@
+package br.com.aula04.beans;
+
+public class ContaCorrente {
+	private int agencia;
+	private int conta;
+	private float saldo;
+	private boolean especial;
+	private Cliente cliente;
+	
+	
+	
+}
